a007362dc1ac9d5eb97c58cc03e8b39363434007
Emma Follender <BrooklynUnited@Brooklyns-iMac.local>
2015-02-09 14:20:46 -0500

    afgdafg

--- server.rb
+++ server.rb
@@ -23,8 +23,10 @@ get '/info/:user' do
 end
 
 post '/award-points' do
+  q = request["text"]
+  puts q
   puts request.body
-  puts JSON.parse(request.body.string)
+  #puts JSON.parse(request.body)
   # q = request[:text]
   # points = /(^[0-9]*)/.match(q)[0].to_i
   # user = /(@[\w]*)/.match(q)[0].to_s.gsub(/[@]/,'')
