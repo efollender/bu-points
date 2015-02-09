2150a4ca01008d23869d952c228222c2865bba20
Emma Follender <BrooklynUnited@Brooklyns-iMac.local>
2015-02-09 16:48:55 -0500

    adfafa

--- server.rb
+++ server.rb
@@ -24,8 +24,8 @@ end
 
 get '/award-points' do
   q = params[:text]
-  points = /(^[0-9]*)/.match(q)[0].to_i
-  user = /(@[\w]*)/.match(q)[0].to_s.gsub(/[@]/,'')
+  points = /(^[0-9]*)/.match(q).to_i
+  user = /(@[\w]*)/.match(q).to_s.gsub(/[@]/,'')
   puts points, user
   res = award_points(user,points,firebase)
   return res
